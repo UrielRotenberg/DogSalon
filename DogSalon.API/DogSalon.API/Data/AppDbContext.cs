6dd0717cfa43d7ace36df08782250b44680e4907
UrielRotenberg <uri314el@gmail.com>
2026-02-17 17:17:04 +0200

    Configured DbContext and CORS in Program.cs

---
+++ DogSalon.API/DogSalon.API/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DogSalon.API.Models;
+
+namespace DogSalon.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+    }
+}
--- DogSalon.API/DogSalon.API/Program.cs
+++ DogSalon.API/DogSalon.API/Program.cs
@@ -1,6 +1,12 @@
+using DogSalon.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
@@ -15,6 +21,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 
 app.UseAuthorization();
 
